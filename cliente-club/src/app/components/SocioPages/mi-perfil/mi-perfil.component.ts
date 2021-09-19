97c0f9a32a420a8c75acff7539030101799a70ae
Mariano Patricio Rosi <you@example.com>
2021-09-19 13:29:37 -0300

    modificar socio funca

--- cliente-club/src/app/components/SocioPages/mi-perfil/mi-perfil.component.ts
+++ cliente-club/src/app/components/SocioPages/mi-perfil/mi-perfil.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SociosService } from 'src/app/services/socios.service';
 import { Router } from '@angular/router';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-mi-perfil',
@@ -10,9 +11,20 @@ import { Router } from '@angular/router';
 export class MiPerfilComponent implements OnInit {
 
   usuarios: any = [];
+  errorNumUs=0;
+  errorNombre=0;
+  errorApellido=0;
+  errorDNI=0;
+  errorMail=0;
+  errorTel=0;
+  errorDirec=0;
+  errorPassword=0;
+
+  reintentar: boolean = false;
+  mensaje: string = "";
   user = { Numero_Usuario: "", Nombre_Usuario: "", Apellido_Usuario: "", DNI_Usuario: "", Mail_Usuario: "", Telefono_Usuario: "", Direccion_Usuario: "", Password_Usuario: "", Id_Rol: "2", Id_Estado: "1" };
 
-  constructor(private sociosService: SociosService, private router: Router) { }
+  constructor(private sociosService: SociosService,private usuariosService: UsuariosService ,private router: Router) { }
 
   ngOnInit(): void {
     let Numero_Usuario = localStorage.getItem('Usuario');
@@ -22,6 +34,18 @@ export class MiPerfilComponent implements OnInit {
         let result: any = res;
         this.usuarios = res;
         console.log(result);
+      },
+      err => console.log(err)
+    )
+  }
+
+  modificarUser(usuario: any) {
+    console.log(usuario);
+    this.usuariosService.actualizarUsuario(usuario.Numero_Usuario, usuario).subscribe(
+      res => {
+        let result: any = res;
+        console.log(result.message);
+        this.ngOnInit();
       },
       err => console.log(err)
     )
