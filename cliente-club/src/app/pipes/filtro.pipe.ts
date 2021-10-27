f3c1b06fc75023444aa8798173024e10bc760bab
Luciano <mosqueralucho@gmail.com>
2021-10-27 16:45:13 -0300

    arreglos

--- cliente-club/src/app/pipes/filtro.pipe.ts
+++ cliente-club/src/app/pipes/filtro.pipe.ts
@@ -8,7 +8,7 @@ export class FiltroPipe implements PipeTransform {
   transform(value: any, args: any, page: number = 0): any {
     const resultPosts = [];
     for (const usuario of value) {
-      if (usuario.Nombre_Usuario.indexOf(args) > -1) {
+      if (usuario.Nombre_Usuario.indexOf(args) > -1 || usuario.Apellido_Usuario.indexOf(args) > -1) {
         resultPosts.push(usuario);
 
       };
